62ad02b49cba92da1b64986cd98f87f7535f4921
shubhamgarg21156 <gargshubham21156@gmail.com>
2021-09-16 20:42:16 +0530

    Changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 //Mongo Database
 const db = require("./config/mongoose");
